c4f213245c30726b34e18af6b5454af42ed76c88
gsvleao <gsvleao@gmail.com>
2020-06-23 16:34:37 -0300

    Programa que lê três números e mostra qual é o maior e qual é o menor

---
+++ Desafio33yt.py
@@ -0,0 +1,47 @@
+"""
+Exercício Python 033:
+Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+"""
+
+print('=-' * 10, 'Desafio 33', '-=' * 10)
+"""
+n1 = int(input('Insira o primeiro número: '))
+n2 = int(input('Insira o segundo número: '))
+n3 = int(input('Insira o terceiro número: '))
+
+if n1 < n2 and n1 < n3:
+    menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+
+if n1 > n2 and n1 > n3:
+    maior = n1
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+
+print('O menor número é {}.'.format(menor))
+print('O maior número é {}.'.format(maior))
+"""
+n1 = int(input('Insira o primeiro número: '))
+n2 = int(input('Insira o segundo número: '))
+n3 = int(input('Insira o terceiro número: '))
+# atribuindo valores antecipadamente pra reduzir o código
+menor = n1
+maior = n1
+
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+
+print('O menor número é {}.'.format(menor))
+print('O maior número é {}.'.format(maior))
